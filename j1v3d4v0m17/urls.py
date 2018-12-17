952dfc01d558bc1110bba3ca1c57a2eb3cb8ee26
Jeremy VINCENT <jeremy.vincent60@gmail.com>
2018-12-17 05:53:29 +0100

    adding basic security and comments system

--- j1v3d4v0m17/urls.py
+++ j1v3d4v0m17/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include, url
-
+from django.contrib.auth import views as auth_views
+ 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'', include('blog.urls')),
 ]
---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/j1v3/www/j1v3d4v0m17/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
